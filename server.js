a87a0068130ad60ccdb6542b4c62e641eef8c62a
Nick Hilton <hilton@csh.rit.edu>
2014-08-25 23:43:02 +0100

    Fixed the server. I had a slash in the wrong place. Also, added an index page for testing

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 
-var publicDir = 'public/';
+var publicDir = '/public';
 
 http.createServer(function(req, res) {
-  if(req.url.indexOf('api') == 0) {
+  console.log(req.url);
+  if(req.url.indexOf('/api') == 0) {
     res.writeHead(200, {"ContentType":"text/plain"});
     res.end('You have reached the API');
   }
   else {
+    console.log(__dirname+publicDir+req.url);
     fs.readFile(__dirname+publicDir+req.url, function(err, data) {
       if(err) {
         res.writeHead(404, {'ContentType':'text/plain'});
